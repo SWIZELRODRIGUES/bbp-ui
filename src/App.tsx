25ae385f7c95761d45e9bfc8e8e71d1c9300ac97
SWIZELRODRIGUES <swizel1997@gmail.com>
2023-04-24 14:43:11 +0530

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button, Skeleton } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <Skeleton variant="rectangular" width={210} height={118} />
+      <Button variant="contained">Hello World</Button>
       </header>
     </div>
   );
